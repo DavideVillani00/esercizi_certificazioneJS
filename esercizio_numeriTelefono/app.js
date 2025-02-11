c14607fc868d02f6cff332d7dc8e5101175fcf45
DavideVillani00 <villani.dado@gmail.com>
2025-02-11 23:50:21 +0100

    completato esercizio

--- esercizio_numeriTelefono/app.js
+++ esercizio_numeriTelefono/app.js
@@ -1,42 +1,22 @@
-const userInput = document.querySelector("#user-input");
-const checkBtn = document.querySelector("#check-btn");
-const clearBtn = document.querySelector("#clear-btn");
-const pResult = document.querySelector("#results-div");
+const userInput = document.getElementById("user-input");
+const checkBtn = document.getElementById("check-btn");
+const clearBtn = document.getElementById("clear-btn");
+const resultsDiv = document.getElementById("results-div");
+const pattern = /^(1\s?)?(\(\d{3}\)|\d{3})([\s-])?\d{3}([\s-])?\d{4}$/;
 
-// ncell = 10 numeri
-// nprefisso all'inizio (1)
-// le parentesi possono circondare solo i primi 3 numeri escluso il prefisso
-
-clearBtn.addEventListener("click", () => (pResult.textContent = ""));
-checkBtn.addEventListener("click", () => {
-  let valid = false;
+checkBtn.addEventListener("click", function () {
   const number = userInput.value;
-  if (number == "") {
-    return alert("Please provide a phone number");
-  }
-  const clearNumber = number.match(/\d/g);
-  const numberLength = clearNumber.length;
-  console.log(clearNumber, numberLength, valid);
-  if (numberLength < 10) {
-    console.log("troppo corto");
-    return (valid = false);
-  }
-  const prefixLength = numberLength - 10;
-  console.log(prefixLength);
 
-  if (prefixLength > 0) {
-    const prefix = clearNumber.slice(0, prefixLength);
-    console.log(prefix);
-    if (prefix.join() != 1) {
-      console.log("prefisso non valido", prefix);
-      return (valid = false);
-    }
-    console.log("prefisso valido");
+  if (!number) {
+    alert("Please provide a phone number");
+    return;
   }
 
-  console.log(clearNumber, valid);
+  const valid = pattern.test(number);
+  resultsDiv.textContent =
+    (valid ? "Valid US number: " : "Invalid US number: ") + number;
+});
 
-  pResult.textContent = valid
-    ? `Valid US number: ${number}`
-    : `Invalid US number: ${number}`;
+clearBtn.addEventListener("click", function () {
+  resultsDiv.textContent = "";
 });
